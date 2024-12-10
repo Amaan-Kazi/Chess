8a0251cd41808667145e56a6384a7fc4a4311e0b
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-11 00:47:41 +0530

    Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
 "use client"
 
 import "./globals.css";
+import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 import { useEffect, useState } from "react";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   // ThemeProvider detects system theme and applies it dynamically
@@ -13,7 +26,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   }, []);
 
   if (!mounted) {
-    return <div></div>;
+    return <div>Loading...</div>;
   }
 
   return (
@@ -23,7 +36,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <meta name="description" content="Chess, play online, locally or with bot, created by Amaan Kazi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={`antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
