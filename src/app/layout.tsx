15d16ed6263bdac2fdff738ecf31c0b2776bc01e
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-11 01:25:22 +0530

    disabled use effect and state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-"use client"
+// "use client"
 
 import "./globals.css";
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,18 +18,18 @@ const geistMono = localFont({
 });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-  // ThemeProvider detects system theme and applies it dynamically
-  const [mounted, setMounted] = useState(false);
+  // // ThemeProvider detects system theme and applies it dynamically
+  // const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
 
-  if (!mounted) {
-    return <div>Loading...</div>;
-  }
+  // if (!mounted) {
+  //   return <div>Loading...</div>;
+  // }
 
-  console.log("Mounted")
+  // console.log("Mounted")
 
   return (
     <html lang="en">
