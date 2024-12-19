cd9e96fb909434e1747e6d5d37779dc5f4d3cd88
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-20 03:31:42 +0530

    Collapsable navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,6 +1,11 @@
+"use client";
 import "../../public/fonts.css";
+import { useState } from "react";
+import { Menu, X } from 'lucide-react';
 
 export default function Navbar({ activePage }: { activePage: string }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  console.log(activePage);
   return (
     <>
       <style>{`
@@ -12,9 +17,35 @@ export default function Navbar({ activePage }: { activePage: string }) {
           background-color: rgb(9, 9, 11);
         }
       `}</style>
-      <div className="mx-auto flex min-w-full navbar-background">
-        <div className="font-academiaM54 text-3xl p-2">CHESS {activePage}</div>
-      </div>
+
+      <nav className="navbar-background shadow-md w-full flex justify-between items-center">
+        <div className="text-3xl font-academiaM54 p-3">Chess</div>
+
+        {/* Button for expanding navbar vertically on small devices */}
+        <button
+          className="block md:hidden mr-3"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <X className="w-6 h-6"/> : <Menu className="w-6 h-6"/>}
+        </button>
+
+        {/* Links */}
+        <div
+          className={`md:flex absolute md:relative top-14 left-0 right-0 navbar-background md:bg-transparent md:top-auto transition-all ${
+            menuOpen ? "block" : "hidden"
+          }`}
+        >
+          <a href="#" className="block px-4 py-2 md:py-0">
+            Home
+          </a>
+          <a href="#" className="block px-4 py-2 md:py-0">
+            About
+          </a>
+          <a href="#" className="block px-4 py-2 md:py-0">
+            Contact
+          </a>
+        </div>
+      </nav>
     </>
   );
 }
