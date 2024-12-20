89bf21e484d18271fd74c13af9e01caf2a20bba2
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-20 18:32:51 +0530

    Theme switcher

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,16 @@
 "use client";
 import "../../public/fonts.css";
+
 import { useState } from "react";
-import { Menu, X } from 'lucide-react';
+import { useTheme } from "next-themes";
+import { Menu, X, Sun, Moon } from 'lucide-react';
 
 export default function Navbar({ activePage }: { activePage: string }) {
+  const { theme, setTheme } = useTheme();
   const [menuOpen, setMenuOpen] = useState(false);
+  
   console.log(activePage);
+
   return (
     <>
       <style>{`
@@ -18,33 +23,43 @@ export default function Navbar({ activePage }: { activePage: string }) {
         }
       `}</style>
 
-      <nav className="navbar-background shadow-md w-full flex justify-between items-center">
-        <div className="text-3xl font-academiaM54 p-3">Chess</div>
+      <nav className="flex navbar-background shadow-md">
+        <div className="w-full flex justify-between items-center">
+          <div className="text-3xl font-academiaM54 p-3">Chess</div>
 
-        {/* Button for expanding navbar vertically on small devices */}
-        <button
-          className="block md:hidden mr-3"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? <X className="w-6 h-6"/> : <Menu className="w-6 h-6"/>}
-        </button>
+          {/* Button for expanding navbar vertically on small devices */}
+          <button
+            className="block md:hidden"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? <X className="w-6 h-6"/> : <Menu className="w-6 h-6"/>}
+          </button>
 
-        {/* Links */}
-        <div
-          className={`md:flex absolute md:relative top-14 left-0 right-0 navbar-background md:bg-transparent md:top-auto transition-all ${
-            menuOpen ? "block" : "hidden"
-          }`}
-        >
-          <a href="#" className="block px-4 py-2 md:py-0">
-            Home
-          </a>
-          <a href="#" className="block px-4 py-2 md:py-0">
-            About
-          </a>
-          <a href="#" className="block px-4 py-2 md:py-0">
-            Contact
-          </a>
+          {/* Links */}
+          <div
+            className={`md:flex absolute md:relative top-14 left-0 right-0 navbar-background md:bg-transparent md:top-auto transition-all ${
+              menuOpen ? "block" : "hidden"
+            }`}
+          >
+            <a href="#" className="block px-4 py-2 md:py-0">
+              Home
+            </a>
+            <a href="#" className="block px-4 py-2 md:py-0">
+              About
+            </a>
+            <a href="#" className="block px-4 py-2 md:py-0">
+              Contact
+            </a>
+          </div>
         </div>
+
+        {/* Theme switch button */}
+        <button
+          className="mx-3"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          {theme === "light" ? <Moon /> : <Sun />}
+        </button>
       </nav>
     </>
   );
