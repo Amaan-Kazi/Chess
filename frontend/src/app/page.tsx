7c9ab10b7e801c084d140a228f3b60b8593e57a0
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-24 03:10:35 +0530

    Reduced nesting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,17 +2,14 @@
 import { useState } from "react";
 
 import Navbar from "@/components/navbar"
-import {Button} from "@/components/ui/button"
+// import {Button} from "@/components/ui/button"
 
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
   DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger,
 } from "@/components/ui/drawer"
 
 import {
@@ -23,12 +20,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-
-function Settings() {
-  return (
-    <>Hello World</>
-  );
-}
 
 export default function Home() {
   const [selection, setSelection] = useState("null");
@@ -41,67 +32,62 @@ export default function Home() {
     <>
       <Navbar activePage="Home"/>
       <div className="container mx-auto flex justify-evenly">
-        <div>
-          <Card className="m-10 min-h-96">
-            <CardHeader>
-              <CardTitle><p className="text-4xl md:text-6xl">Game Mode</p></CardTitle>
-            </CardHeader>
+        <Card className="m-10 min-h-96">
+          <CardHeader>
+            <CardTitle><p className="text-4xl md:text-6xl">Game Mode</p></CardTitle>
+          </CardHeader>
 
-            <CardContent>
-              <div className="flex flex-col gap-10">
-                <Card
-                  className="cursor-pointer"
-                  onClick={() => setSelection("PlayOnline")}
-                >
-                  <CardHeader>
-                    <CardTitle>Play Online</CardTitle>
-                    <CardDescription>Play against other players online</CardDescription>
-                  </CardHeader>
-                </Card>
+          <CardContent>
+            <div className="flex flex-col gap-10">
+              <Card
+                className="cursor-pointer"
+                onClick={() => setSelection("PlayOnline")}
+              >
+                <CardHeader>
+                  <CardTitle>Play Online</CardTitle>
+                  <CardDescription>Play against other players online</CardDescription>
+                </CardHeader>
+              </Card>
 
-                <Card
-                  className="cursor-pointer"
-                  onClick={() => setSelection("PlayWithBot")}
-                >
-                  <CardHeader>
-                    <CardTitle>Play with Bot</CardTitle>
-                    <CardDescription>Play against a bot with customizable difficulty</CardDescription>
-                  </CardHeader>
-                </Card>
+              <Card
+                className="cursor-pointer"
+                onClick={() => setSelection("PlayWithBot")}
+              >
+                <CardHeader>
+                  <CardTitle>Play with Bot</CardTitle>
+                  <CardDescription>Play against a bot with customizable difficulty</CardDescription>
+                </CardHeader>
+              </Card>
 
-                <Card
-                  className="cursor-pointer"
-                  onClick={() => setSelection("PassAndPlay")}
-                >
-                  <CardHeader>
-                    <CardTitle>Pass and Play</CardTitle>
-                    <CardDescription>Play against another player locally</CardDescription>
-                  </CardHeader>
-                </Card>
-              </div>
-            </CardContent>
-          </Card>
-        </div>
+              <Card
+                className="cursor-pointer"
+                onClick={() => setSelection("PassAndPlay")}
+              >
+                <CardHeader>
+                  <CardTitle>Pass and Play</CardTitle>
+                  <CardDescription>Play against another player locally</CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+          </CardContent>
+        </Card>
 
-        <div className="hidden md:block">
-          {/* Game Settings */}
-          {/* Hidden on small screen and replaced by drawer */}
-          
-          <Card>
-            <CardHeader>
-              <CardTitle><p className="text-4xl">Settings</p></CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
+        {/* Game Settings */}
+        {/* Hidden on small screen and replaced by drawer */}
+        <Card className="hidden md:block">
+          <CardHeader>
+            <CardTitle><p className="text-4xl">Settings</p></CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
 
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
 
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-        </div>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
 
         <Drawer open={selection !== "null"} onOpenChange={selectionChange}>
           <DrawerContent>
@@ -115,6 +101,7 @@ export default function Home() {
     </>
   );
 }
+
 // select play, play with bot, play online
 // drawer, side thing, select settings
 // store settings in local storage
