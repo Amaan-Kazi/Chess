baa3a3de6835bea2bfcadde6198e64587b5bacc2
Amaan Kazi <amaankazi1793@gmail.com>
2024-12-28 16:15:11 +0530

    Board Class

---
+++ frontend/src/chess/board.ts
@@ -0,0 +1,45 @@
+class Board {
+  grid: (string | null)[][];
+  castlingRights: { white: { kingSide: boolean; queenSide: boolean }, black: { kingSide: boolean; queenSide: boolean } };
+  enPassant: string | null; // e.g., "e3" if black just moved a pawn two squares forward.
+  halfMoveClock: number;
+  fullMoveNumber: number;
+  gameState: "ongoing" | "checkmate" | "stalemate" | "draw";
+
+  constructor(copyBoard?: Board) {
+    if (!copyBoard) {      
+      this.grid = [
+        ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+        Array(8).fill('p'),
+        ...Array(4).fill(Array(8).fill(null)), // Four rows of null
+        Array(8).fill('P'),
+        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+      ];
+
+      this.castlingRights = {
+        white: { kingSide: true, queenSide: true },
+        black: { kingSide: true, queenSide: true },
+      };
+
+      this.enPassant = null;
+      this.halfMoveClock = 0;
+      this.fullMoveNumber = 1;
+      this.gameState = "ongoing";
+    }
+    else {
+      this.grid = JSON.parse(JSON.stringify(copyBoard.grid)); // Deep copy of the grid
+
+      this.castlingRights = {
+        white: { kingSide: copyBoard.castlingRights.white.kingSide, queenSide: copyBoard.castlingRights.white.queenSide },
+        black: { kingSide: copyBoard.castlingRights.black.kingSide, queenSide: copyBoard.castlingRights.black.queenSide },
+      };
+
+      this.enPassant = copyBoard.enPassant;
+      this.halfMoveClock = copyBoard.halfMoveClock;
+      this.fullMoveNumber = copyBoard.fullMoveNumber;
+      this.gameState = copyBoard.gameState;
+    }
+  }
+}
+
+export { Board };
