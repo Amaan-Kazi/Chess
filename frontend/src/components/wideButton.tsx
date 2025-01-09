fc0975faedacb6386c80a688de5fcb6b7ed65fd6
Amaan Kazi <amaankazi1793@gmail.com>
2025-01-10 02:13:26 +0530

    Wide Button [Non Highlighted]

--- frontend/src/components/wideButton.tsx
+++ frontend/src/components/wideButton.tsx
@@ -11,13 +11,13 @@ export const WideButtonImage: React.FC<{ src: string, alt?: string, className?: 
 
 export const WideButtonTitle: React.FC<{ children: ReactNode, className?: string }> = ({ children, className }) => {
   return (
-    <div className={`text-left text-3xl font-bold ${className}`}>{children}</div>
+    <div className={`text-left text-3xl font-bold text-white ${className}`}>{children}</div>
   );
 };
 
 export const WideButtonDescription: React.FC<{ children: ReactNode, className?: string }> = ({ children, className }) => {
   return (
-    <div className={`text-left text-sm ${className}`}>{children}</div>
+    <div className={`text-left text-sm text-white ${className}`}>{children}</div>
   );
 };
 
@@ -42,7 +42,12 @@ export const WideButton: React.FC<WideButtonProps> = ({ onClick, children, class
   return (
     <button
       onClick={onClick}
-      className={`flex items-center p-4 w-full max-w-lg bg-[hsl(84,55%,60%)] hover:bg-[hsl(84,55%,60%)] rounded-lg shadow-[0px_4px_0px_rgba(84,140,67,1)] hover:shadow-[0px_3px_0px_rgba(84,140,67,1)] ${className}`}
+      className={`
+        flex items-center p-4 w-full max-w-lg rounded-lg
+        ${ highlighted && "bg-[hsl(90,42%,51%)] hover:bg-[hsl(84,55%,60%)] shadow-[0px_4px_0px_rgba(69,117,60,1)]"}
+        ${!highlighted && "bg-[hsl(30,3%,26%)]  hover:bg-[hsl(45,3%,29%)]  shadow-[0px_4px_0px_rgba(0,0,0,0.1)]"}
+        ${className}
+      `}
     >
       {image}
       <div className="flex-1 ml-4">
