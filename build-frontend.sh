de723390a57da69cf2da552cefed4df1d89c486f
Amaan Kazi <amaankazi1793@gmail.com>
2025-05-01 18:55:10 +0530

    Documentation

---
+++ build-frontend.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+# Documentation
+printf "\n\n----------------------------------\n"
+printf "Building Documentation [VitePress]"
+printf "\n----------------------------------\n\n"
+
+cd docs
+npx vitepress build
+cd ..
+
+
+# Copying documentation to frontend
+printf "\n\n---------------------------------\n"
+printf "Copying documentation to frontend"
+printf "\n---------------------------------\n\n"
+
+rm -rf frontend/public/docs
+mkdir -p frontend/public/docs
+cp -r docs/.vitepress/dist/* frontend/public/docs
+
+
+# Frontend
+printf "\n\n--------------------------\n"
+printf "Building Frontend [NextJS]"
+printf "\n--------------------------\n\n"
+
+cd frontend
+npm install
+npm run build
+cd ..
+
+
+printf "\n\n---------------------------------------\n"
+printf "Build completed. Output in frontend/out"
+printf "\n---------------------------------------\n\n"
+
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Chess"
+  tagline: A feature rich chess website with online play, bot and local play
+  actions:
+    - theme: brand
+      text: Documentation
+      link: /overview
+    - theme: alt
+      text: Build Instructions
+      link: /build-instructions
+
+features:
+  - title: Play Online
+    details: Play with anyone on any device, anywhere in the world
+  - title: Play with Bot
+    details: Play with a customizable bot using the minimax algorithm
+  - title: Pass and Play
+    details: Play fully customizable games on the same device
+---
+
